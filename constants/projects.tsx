690bdf63f8a9b601bc19677f5997dd6d50324c05
Hariswar <bkhariswar@gmail.com>
2025-05-15 10:09:25 -0500

    started working with job, project & social

---
+++ constants/projects.tsx
@@ -0,0 +1,42 @@
+export const projects = [
+  {
+  title: "XYZ",
+    description: "XYZ",
+    image: "", //upload the image link here
+    technologies: ["XYZ", "XYZ", "XYZ", "XYZ"],
+    githubUrl: "", // upload the github link here
+    liveUrl: "" // upload the live link here
+  }, 
+  {
+    title: "XYZ",
+    description: "XYZ",
+    image: "", //upload the image link here
+    technologies: ["XYZ", "XYZ", "XYZ", "XYZ"],
+    githubUrl: "", // upload the github link here
+    liveUrl: "" // upload the live link here
+  },
+  {
+    title: "XYZ",
+    description: "XYZ",
+    image: "", //upload the image link here
+    technologies: ["XYZ", "XYZ", "XYZ", "XYZ"],
+    githubUrl: "", // upload the github link here
+    liveUrl: "" // upload the live link here
+  },
+  {
+    title: "XYZ",
+    description: "XYZ",
+    image: "", //upload the image link here
+    technologies: ["XYZ", "XYZ", "XYZ", "XYZ"],
+    githubUrl: "", // upload the github link here
+    liveUrl: "" // upload the live link here
+  },
+  {
+    title: "XYZ",
+    description: "XYZ",
+    image: "", //upload the image link here
+    technologies: ["XYZ", "XYZ", "XYZ", "XYZ"],
+    githubUrl: "", // upload the github link here
+    liveUrl: "" // upload the live link here
+  }
+]; 
---
+++ constants/socials.tsx
@@ -0,0 +1,18 @@
+export const socials = [
+  {
+    name : 'GitHub',
+    link : '' // Add the Github link here
+  },
+  {
+    name : 'LinkedIn',
+    link : '' // Add the LinkedIn link here
+  },
+  {
+    name : 'Instagram',
+    link : '' // Add the Twitter link here
+  },
+  {
+    name : 'Email',
+    link : '' // Add the Facebook link here
+  }
+]
