f7f7483fbc4cf8af0989ae10cfd8cbf07dab9087
Hariswar <bkhariswar@gmail.com>
2025-05-24 16:04:13 -0500

    fixing bugs

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <h1 className="font-bold text-teal-500 text-2xl">
+    <h1 className="font-bold text-teal-500 text-3xl">
       Hello World -- Check
     </h1>
   )
