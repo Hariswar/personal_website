80b1bc839063891b60a4b3ad43170146914d5065
Hariswar <bkhariswar@gmail.com>
2025-12-23 16:28:17 -0600

    included jobexperience structure

---
+++ personal_website/src/constants/jobexperience.tsx
@@ -0,0 +1,29 @@
+export const jobExperience = [
+ {
+  title: "",
+  company: "",
+  location: "",
+  dates: "",
+  description: "",
+  technologies: "",
+  icon: "", 
+ },
+ {
+  title: "",
+  company: "",
+  location: "",
+  dates: "",
+  description: "",
+  technologies: "",
+  icon: "",
+ },
+ {
+  title: "",
+  company: "",
+  location: "",
+  dates: "",
+  description: "",
+  technologies: "",
+  icon: "",
+ }
+]
