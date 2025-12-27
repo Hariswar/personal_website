77781f579902568136848cb83a67b5d397742497
Hariswar <bkhariswar@gmail.com>
2025-12-27 11:43:36 -0600

    started with listing the experiences

---
+++ hariswarWebsite/src/constants/experience.ts
@@ -0,0 +1,20 @@
+export const jobExperiences = [
+  {
+    role: "",
+    company: "",
+    location: "",
+    period: "",
+    description: "",
+    technologies: [],
+    icon: ""
+   },
+   {
+    role: "",
+    company: "",
+    location: "",
+    period: "",
+    description: "",
+    technologies: [],
+    icon: ""
+   }
+]; 
