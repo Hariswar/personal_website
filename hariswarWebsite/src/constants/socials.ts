d06f7918dc2d5e7b53abf535711c155b52cafb4b
Hariswar <bkhariswar@gmail.com>
2025-12-26 18:39:16 -0600

    started the socials

---
+++ hariswarWebsite/src/constants/socials.ts
@@ -0,0 +1,18 @@
+export const socials = [
+  {
+    platform: "Github",
+    url: "https://github.com/Hariswar"
+  },
+  {
+    platform: "LinkedIn",
+    url: "https://www.linkedin.com/in/hariswar-baburaj/"
+  },
+  {
+    platform: "Instagram",
+    url: "https://instagram.com"
+  },
+  {
+    platform: "Email",
+    url: "mailto:bkhariswar@gmail.com"
+  }
+];
