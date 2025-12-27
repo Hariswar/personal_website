13c2d965ec0d79750b9d0b3453a72e75bf2d6d63
Hariswar <bkhariswar@gmail.com>
2025-12-27 14:29:48 -0600

    close to finishing up the job experiences

--- hariswarWebsite/src/constants/experience.ts
+++ hariswarWebsite/src/constants/experience.ts
@@ -1,5 +1,14 @@
 export const jobExperiences = [
   {
+    title: "Grader - Theory of Computer Science",
+    company: "Missouri University of Science and Technology",
+    location: "Rolla, MO",
+    period: "August 2025 - Decemeber 2025",
+    description: "Developing autograders and test cases for homework and assignments, allowing students to receive instant feedback by successfully running through the test cases and able to see their scores for the assignment immediately. Reducing manual grading effort by 80% for the professors and graders. Working with other grader to brainstorm and create homework questions to make sure students have been able to understand key concepts.", 
+    technologies: ["Python", "Github"],
+    icon: ""
+   },
+   {
     role: "",
     company: "",
     location: "",
