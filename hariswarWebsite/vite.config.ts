f88b0bf5684b4f6e3925f6e590deb02895ccda5c
Hariswar <bkhariswar@gmail.com>
2026-01-12 13:27:35 -0600

    made changes in order to work for vercell deployment

--- hariswarWebsite/vite.config.ts
+++ hariswarWebsite/vite.config.ts
@@ -9,4 +9,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'), // <- this makes `@/` point to src/
     },
   },
+  build: {
+    outDir: 'build', // <- change output folder from 'dist' to 'build'
+  },
 })
