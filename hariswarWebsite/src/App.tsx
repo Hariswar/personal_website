10471da97312aab5820915094b670a4e6d5a3c4b
Hariswar <bkhariswar@gmail.com>
2026-01-12 10:47:44 -0600

    finished

--- hariswarWebsite/src/App.tsx
+++ hariswarWebsite/src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { TooltipProvider } from "@/components/ui/tooltip"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { ThemeProvider } from "@/Theme/darkLight"
+import Page from "./Page"
+import ResearchPaper from "./pages/ResearchPaper"
+import ResearchPaperDetail from "./pages/ResearchPaperDetail"
+import NotFound from "./pages/Failed"
+import "./App.css"
 
-function App() {
-  const [count, setCount] = useState(0)
+const queryClient = new QueryClient()
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+// List research paper and its details 
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider>
+      <TooltipProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Page />} />
+            <Route path="/research-paper" element={<ResearchPaper />} />
+            <Route path="/research-paper/:id" element={<ResearchPaperDetail />} />
+            {}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </ThemeProvider>
+  </QueryClientProvider>
+)
 
 export default App
