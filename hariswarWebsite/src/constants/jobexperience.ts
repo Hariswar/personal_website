11d5e1c201dad2be14772692e5f0119c02ed3bda
Hariswar <bkhariswar@gmail.com>
2026-01-13 10:25:26 -0600

    filling up the details

--- hariswarWebsite/src/constants/jobexperience.ts
+++ hariswarWebsite/src/constants/jobexperience.ts
@@ -9,7 +9,16 @@ export const jobExperiences = [
     icon: ""
    },
    {
-    title: "",
+    title: "Undergraduate Research Assistant",
+    company: "Missouri University of Science and Technology",
+    location: "Rolla, MO",
+    period: "Feb 2025 - May 2025",
+    description: "Volunteered to work with Dr. Xiong Zhang in a research on improving triaxial soil testing accuracy using the photogrammetry based methods. Assisted the research by generating the 2D results of the sand pictures from photographs which helped us to create a base for developing 3D models. Created a 3D models from 10+ different photo angles of the triaxial soil specimen developed with 3D models based on the different angles of pictures. The goal was to improve the accuracy of the point measurements and overall model accuracy.",
+    technologies: ["Python", "MATLAB", "CAD", "Github"],
+    icon: ""
+   },
+   {
+    tutle: "",
     company: "",
     location: "",
     period: "",
