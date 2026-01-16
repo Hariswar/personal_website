ca4c0d9e37b031e204c690ef4272ea192bf1baf5
Hariswar <bkhariswar@gmail.com>
2026-01-15 18:25:08 -0600

    filling out research stuff

--- hariswarWebsite/src/constants/researchPapers.ts
+++ hariswarWebsite/src/constants/researchPapers.ts
@@ -3,7 +3,7 @@ export const researchPapers = [
     id: 1,
     title: "The Progression of Home Console Operating Systems",
     description: "We researched the usage of different Operating Systems for different console game companies. With every new console generation comes the debate of which game console is the “best”. Rather than using subjective and biased opinions, we aim to explore and uncover what difference in their operating system sets them apart. We will be comparing the performance, memory management, and scheduling of each console’s Operating System. From these parts, we hope to find the reason for each console's performance and how these decisions impact the overall system performance and user satisfaction.",
-    image: "",
+    image: "/osPaper.png",
     abstract: "Operating systems (OSs) serve the critical role of connecting the hardware and software of a device. As the hardware continues to improve and the needs of the software continue to rise, OSs must improve to meet these needs. The gaming industry serves as an interesting microcosm of this three-way race. By researching the three major gaming companies, Nintendo, Sony, and Microsoft, we can analyze how the progression of hardware and each company's goals impacted the OSs they designed. We found that Sony built its OSs with an emphasis on maximum performance, Microsoft built theirs off of Windows for both compatibility and ease of implementation, and Nintendo built theirs to be as minimal as possible to support cheaper and more accessible hardware.",
     authors: ["Gavin Sutherland", "Jason Ni", "Hariswar Baburaj"],
     publicationDate: "April 2025",
