21936876214ca89af96db0c80e8fe70981f78831
Hariswar <bkhariswar@gmail.com>
2026-06-11 21:07:06 -0500

    included certifications

--- hariswarWebsite/src/constants/certifications.ts
+++ hariswarWebsite/src/constants/certifications.ts
@@ -1,14 +1,34 @@
 export const certifications = [
   {
-    title: "Building RAG Agents with LLMs",
+    title: "HackerRank - Python Certificate",
     url: "#",
   },
   {
-    title: "Fundamentals of Accelerated Computing with CUDA Python",
+    title: "Fundamentals of Deep Learning",
+    url: "#",
+  },
+  {
+    title: "Getting Started with Deep Learning",
+    url: "#",
+  },
+  {
+    title: "HackerRank - Java Certificate",
+    url: "#",
+  },
+  {
+    title: "Hack-Nation Certificate",
+    url: "#",
+  },
+    {
+    title: "UK AI Agent Hackathon - TOP 15",
     url: "#",
   },
   {
     title: "Disaster Risk Monitoring Using Satellite Imagery",
+    url: "#",
+  },
+    {
+    title: "Building RAG Agents with LLMs",
     url: "#",
   },
 ];
